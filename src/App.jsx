13c9ce44c9102eef8c25af16b270f51f2c8cc31f
Mriganka Gogoi <gogoimriganka09@gmail.com>
2023-08-03 08:25:11 +0530

    Starter filed added completely

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<Login />} />
         <Route path="product" element={<Product />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
