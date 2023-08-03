7b982ddc56273c6c7fec106a74f6f0bb46d61252
Mriganka Gogoi <gogoimriganka09@gmail.com>
2023-08-03 07:12:08 +0530

    First Commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function HomePage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+    </div>
+  );
+}
+
+export default HomePage;
